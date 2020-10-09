080cf6986e980d35c433be8c6cdb743d8f97af0a
Dena Neuman <denaweiss5@gmail.com>
2020-10-09 20:46:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
   def my_hash_creator(key, value)
-    {name=> 'Grace Hopper'}
-    my_hash_creator.new = {1=> 2}
+    {key => value}
+    #{name=> 'Grace Hopper'}
+    #my_hash_creator.new = {1=> 2}
 end
 
 def read_from_hash(hash, key)
