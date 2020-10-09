352dd063384e68c94620c4d6e1e4815230e46808
Dena Neuman <denaweiss5@gmail.com>
2020-10-09 19:50:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,6 @@
-def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+def my_hash_creator(:name, "Grace Hopper")
+  Hash= {:name => "Grace Hopper"}
+Hash
 end
 
 def read_from_hash(hash, key)
